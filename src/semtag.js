a93ef4ce6cb247865cd989bf7e225b8592173fd0
EivindEE <eivind.elseth@gmail.com>
2012-09-03 13:56:03 +0200

    Added surround() functionality

--- src/semtag.js
+++ src/semtag.js
@@ -1,3 +1,4 @@
+/*jslint browser: true */
 var semtag = function (text, trigger) {
 	"use strict";
 	if (!(text && trigger)) {
@@ -6,12 +7,10 @@ var semtag = function (text, trigger) {
 	var textfield = document.getElementById(text),
 		button = document.getElementById(trigger),
 		self = {};
-
 	self.test = function () {
 		return true;
 	};
 	self.textfield = textfield;
-	
 	self.ancestorOrSelf = function (ancestor, descendant) {
 		if (ancestor === descendant) {
 			return true;
@@ -19,9 +18,17 @@ var semtag = function (text, trigger) {
 		if (descendant.parentNode) {
 			return this.ancestorOrSelf(ancestor, descendant.parentNode);
 		}
-		
 		return false;
 	};
-
+	self.surround = function (fragment, type, cName) {
+		if (!fragment) {
+			throw {name: "MissingArgumentsException", message: "Function requires a range object to surround with tags"};
+		}
+		var tag = document.createElement(type);
+		tag.appendChild(fragment);
+		tag.className =  cName || "";
+		
+		return tag;
+	};
 	return self;
 };
