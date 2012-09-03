dd039f91711057af3e2cea81f9f6f1e2feb933d6
EivindEE <eivind.elseth@gmail.com>
2012-09-03 14:34:17 +0200

    Removed unused code

--- src/semtag_extractor.js
+++ src/semtag_extractor.js
@@ -4,13 +4,7 @@ var semtag = function (text, trigger) {
 	if (!(text && trigger)) {
 		throw {name : "MissingArgumentsException", message : "Function requires both a valid text and trigger argument"};
 	}
-	var textfield = document.getElementById(text),
-		button = document.getElementById(trigger),
-		self = {};
-	self.test = function () {
-		return true;
-	};
-	self.textfield = textfield;
+
 	self.ancestorOrSelf = function (ancestor, descendant) {
 		if (ancestor === descendant) {
 			return true;
