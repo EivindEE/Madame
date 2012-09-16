8c62d2155682c5290cedb06f2d542026ea8f76f4
EivindEE <eivind.elseth@gmail.com>
2012-09-16 14:17:58 +0200

    Moved spec and src to more natural places in public

--- app.js
+++ app.js
@@ -28,6 +28,7 @@ app.configure('development', function(){
 });
 
 app.get('/', routes.index);
+app.get('/test', routes.test);
 app.get('/users', user.list);
 
 http.createServer(app).listen(app.get('port'), function(){
