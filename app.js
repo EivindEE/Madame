f69773d9cbe3d8d0e6f96249977ce4df6e5eb4bd
EivindEE <eivind.elseth@gmail.com>
2012-09-24 14:35:43 +0200

    Added new Single word st view

--- app.js
+++ app.js
@@ -28,6 +28,7 @@ app.configure('development', function(){
 
 app.get('/', routes.index);
 app.get('/test', routes.test);
+app.get('/sw', routes.sw);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log("Express server listening on port " + app.get('port'));
--- routes/index.js
+++ routes/index.js
@@ -6,6 +6,11 @@
 exports.index = function(req, res){
   res.render('semtag', { title: 'SemTag' });
 };
+
+exports.sw = function(req, res){
+  res.render('sw', { title: 'Single Word SemTag' });
+};
+
 exports.test = function(req, res){
   res.render('testrunner', {title: 'SpecRunner', specs: ['javascripts/spec/semtag_spec.js'], sources: ['javascripts/semtag.js'] });
 };
