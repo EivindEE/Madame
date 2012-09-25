ff5cc2c7a88844ce2d91e7f6eaea36a9154b5c76
EivindEE <eivind.elseth@gmail.com>
2012-09-25 10:39:10 +0200

    Added proxy to query lexitags from webpage

---
+++ app/lexitag.js
@@ -0,0 +1,20 @@
+var http = require("http"),
+	url = require('url');
+
+
+exports.lexitag = function (req, res) {
+	var body = "",
+		q = url.parse(req.url, true);
+	
+	http.get('http://lexitags.dyndns.org:8080/server/lexitags2/Semtags' + q.search, function (inner_res) {
+		inner_res.on('data', function (chunk) {
+			body += chunk;
+		});
+		inner_res.on('end', function () {
+			res.write(body);
+			res.end();
+		});
+	}).on('error', function (e) {
+		console.err("Got error: " + e.message);
+	});
+}
