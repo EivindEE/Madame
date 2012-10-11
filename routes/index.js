899cf79f74ba54ab4f3ec0b057b128405765a683
EivindEE <eivind.elseth@gmail.com>
2012-10-11 11:42:15 +0200

    Fixed bug caused by js declaration sequence

--- routes/index.js
+++ routes/index.js
@@ -4,11 +4,11 @@
  */
 
 exports.index = function(req, res){
-  res.render('semtag', { title: 'SemTag', scripts: ['javascripts/dist/SemTag.min.js', 'http://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.min.js']});
+  res.render('semtag', { title: 'SemTag', scripts: ['http://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.min.js', 'javascripts/dist/SemTag.min.js']});
 };
 
 exports.sw = function(req, res){
-  res.render('sw', { title: 'Single Word SemTag', scripts: ['javascripts/dist/SemTag.min.js', 'http://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.min.js']});
+  res.render('sw', { title: 'Single Word SemTag', scripts: ['http://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.min.js', 'javascripts/dist/SemTag.min.js']});
 };
 
 exports.test = function(req, res){
