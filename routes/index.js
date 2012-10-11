702115d556993125b13296b4260000b1e500751e
EivindEE <eivind.elseth@gmail.com>
2012-10-11 10:41:02 +0200

    Updated templates

--- routes/index.js
+++ routes/index.js
@@ -4,13 +4,13 @@
  */
 
 exports.index = function(req, res){
-  res.render('semtag', { title: 'SemTag' });
+  res.render('semtag', { title: 'SemTag', scripts: ['javascripts/dist/SemTag.min.js', 'http://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.min.js']});
 };
 
 exports.sw = function(req, res){
-  res.render('sw', { title: 'Single Word SemTag' });
+  res.render('sw', { title: 'Single Word SemTag', scripts: ['javascripts/dist/SemTag.min.js', 'http://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.min.js']});
 };
 
 exports.test = function(req, res){
-  res.render('testrunner', {title: 'SpecRunner', specs: ['javascripts/spec/semtag_spec.js'], sources: ['javascripts/dist/SemTag.min.js'] });
+  res.render('testrunner', {title: 'SpecRunner', specs: ['javascripts/spec/semtag_spec.js'], sources: ['javascripts/semtag.js', 'http://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.min.js'] });
 };
