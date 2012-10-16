ca1f57a9fbefd723dde8845ada579672461d61e5
EivindEE <eivind.elseth@gmail.com>
2012-10-16 15:51:54 +0200

    Switched from min for debugging

--- routes/index.js
+++ routes/index.js
@@ -8,7 +8,7 @@ exports.index = function(req, res){
 };
 
 exports.sw = function(req, res){
-  res.render('sw', { title: 'Single Word SemTag', scripts: ['http://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.min.js', 'javascripts/dist/SemTag.min.js']});
+  res.render('sw', { title: 'Single Word SemTag', scripts: ['http://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.min.js', 'javascripts/dist/SemTag.js']});
 };
 
 exports.test = function(req, res){
