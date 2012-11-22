7e02a6cb64f832542195b4e76f0f72aa25770cfe
EivindEE <eivind.elseth@gmail.com>
2012-11-22 13:27:34 +0100

    Bugfix

--- app/export/index.js
+++ app/export/index.js
@@ -18,11 +18,11 @@ exports.save = function (body, callback) {
 		} else {
 			body = body.replace(/<img [^>]*class="removeIcon"[^>]*>/g, ''); // Removes x images
 			body = body.replace(/(<span [^>]*class="tagged"[^>]* about="[^#]*)([^>]*">)/g, '$1load?q=' + doc.id + '$2'); // Removes x images
-			Document.update({'_id': doc.id}, {$set: {'body': body}}, {upsert: true}, function (error, newDoc) {
+			Document.update({'_id': doc.id}, {$set: {'body': body}}, {upsert: true}, function (error) {
 				if (error) {
 					callback(error);
 				}
-				callback(null, newDoc.id);
+				callback(null, doc.id);
 			});
 		}
 	});
