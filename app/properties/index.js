59240c4feb37e53e735dc7ab1b6a74df70dd7bd9
EivindEE <eivind.elseth@gmail.com>
2012-11-30 11:10:14 +0100

    Added a properties file which should contain x-file info and volatile info

---
+++ app/properties/index.js
@@ -0,0 +1,5 @@
+exports.dbLocation = 'mongodb://localhost:3001/pages';
+exports.intertalCommentStart = /<!\-\-SCRIPT/g;
+exports.intertalCommentEnd = /SCRIPT\-\->/g;
+exports.htmlCommentStart = /SEMTAG-START-COMMENT/g;
+exports.htmlCommentEnd = /SEMTAG-END-COMMENT/g;
