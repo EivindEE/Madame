7fc3d7cd1e67fed0f203c4c851928461d3b48278
EivindEE <eivind.elseth@gmail.com>
2012-12-11 11:59:26 +0100

    Removed check for allowed types. This should be decided by the user

--- app/schema.properties/index.js
+++ app/schema.properties/index.js
@@ -3,22 +3,9 @@ var mapping = require('./mapping').declaration;
 exports.propertiesList = function (term, callback) {
 	var propertyList = mapping.types[term].properties,
 		propertiesAndTypes = {},
-		i,
-		j,
-		types,
-		type,
-		allowedTypes = ['Text', 'URL'];
+		i;
 	for (i = 0; i < propertyList.length; i += 1) {
-		types = mapping.properties[propertyList[i]].ranges;
-		for (j = 0; j < types.length; j += 1) {
-			type = types[j];
-			if (allowedTypes.indexOf(type) !== -1) {
-				if (!propertiesAndTypes[propertyList[i]]) {
-					propertiesAndTypes[propertyList[i]] = [];
-				}
-				propertiesAndTypes[propertyList[i]].push(type);
-			}
-		}
+		propertiesAndTypes[propertyList[i]] = mapping.properties[propertyList[i]].ranges;
 	}
 	callback(null, propertiesAndTypes);
 };
