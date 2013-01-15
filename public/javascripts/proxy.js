f3ab1652155441a084eed9f14b0c68d4161cea20
EivindEE <eivind.elseth@gmail.com>
2013-01-15 11:55:32 +0100

    Added listener to keypress to allow enterpress for triggering proxy

--- public/javascripts/proxy.js
+++ public/javascripts/proxy.js
@@ -13,22 +13,29 @@ proxy.get = function (url, callback) {
 		callback(data);
 	});
 };
-
-$('#get').click(function () {
+(function () {
 	'use strict';
-	var url = $('#url').val();
-	proxy.get(url, function (error, dom) {
-		if (error) {
-			document.getElementById('content').innerHTML = '<div class="span6"><h4>No such page found</h4></div>';
-		} else {
-			semtag.dom = {};
-			semtag.dom.URI = dom.URI || '';
-			semtag.dom.head = dom.head || '';
-			document.getElementById('content').innerHTML = dom.body;
-			$(document.getElementById('content')).click(function (e) {
-				e.preventDefault();
-			});
-		}
+	var getPage = function () {
+		var url = $('#url').val();
+		proxy.get(url, function (error, dom) {
+			if (error) {
+				document.getElementById('content').innerHTML = '<div class="span6"><h4>No such page found</h4></div>';
+			} else {
+				semtag.dom = {};
+				semtag.dom.URI = dom.URI || '';
+				semtag.dom.head = dom.head || '';
+				document.getElementById('content').innerHTML = dom.body;
+				$(document.getElementById('content')).click(function (e) {
+					e.preventDefault();
+				});
+			}
 
+		});
+	};
+	$('#get').click(getPage);
+	$('#url').keypress(function (e) {
+		if (e.keyCode === 13) {
+			getPage();
+		}
 	});
-});
+}());
