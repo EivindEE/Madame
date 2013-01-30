e81a678909d1f5938bed19be7026cc9352e00ddd
EivindEE <eivind.elseth@gmail.com>
2013-01-30 11:49:18 +0100

    Wrote an algorithm for comparing the results of the two algorithms

---
+++ compareAlgs.js
@@ -0,0 +1,36 @@
+"use strict";
+var fs = require('fs'),
+	wn = require('./app/wn'),
+	synsets = require('./testing/synsets.js').synsets,
+	index = 0,
+	writeSense = function (i) {
+		if (i < synsets.length){
+		wn.bestFit(synsets[i], function (err, json) {
+				if (err) {
+					console.log('Got err for synset: ' + synsets[i]);
+				} else {
+					var result = 'synset: ' + json.synset + '\r';
+					if (json.lin.schema_dot_org) {
+						result += '\tlin: ' + json.lin.schema_dot_org  + '\r';
+					} else {
+						result += '\tlin: None\r';
+					}
+					if (json.rec.schema_dot_org) {
+						result += '\trec: ' + json.rec.schema_dot_org  + '\r';
+					} else {
+						result += '\trec: None\r';
+					}
+					if (json.rec.schema_dot_org !== json.lin.schema_dot_org) {
+						result += 'Different\r';
+					} else {
+						result += 'Same\r';
+					}
+					console.log('Wrote result for ' + json.synset);
+					fs.appendFile('testing/compare', result, function () {
+						writeSense(i+1);
+					});
+				}
+			});
+		}
+	};
+	writeSense(index);
