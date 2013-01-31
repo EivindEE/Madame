cb4492d3ab5017c029e27819938a979df592317a
EivindEE <eivind.elseth@gmail.com>
2013-01-31 14:45:27 +0100

    Added script for finding the levels the different schemadotorg term are at

---
+++ schemaLevels.js
@@ -0,0 +1,28 @@
+"use strict";
+var http = require('http'),
+	fs = require('fs'),
+	findTypeDepths = function (json) {
+		var level,
+			type;
+		console.log(json.types);
+		for (type in json.types){
+			if (json.types.hasOwnProperty(type)){
+				level = json.types[type].ancestors.length;
+				fs.appendFileSync('testing/schemaLevels', '\t' + type + ':' + level + ',\n');
+			}
+		}
+	};
+
+http.get('http://schema.rdfs.org/all.json', function (response) {
+	var body = '';
+	if (response.statusCode !== 200) {
+		console.log('Response not 200');
+	} else {
+		response.on('data', function (chunk) {
+			body += chunk;
+		});
+		response.on('end', function () {
+			findTypeDepths(JSON.parse(body))
+		});
+	}
+})
