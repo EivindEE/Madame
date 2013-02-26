8a6e0599ecc43abafaed16990b90cdeec24265b4
Eivind Eidheim Elseth <eivind.elseth@gmail.com>
2013-02-26 09:28:54 +0100

    Added the specific_properties of the type. Embarasing bug

--- app/schema.properties/index.js
+++ app/schema.properties/index.js
@@ -5,6 +5,9 @@ exports.propertiesList = function (term, callback) {
 	var propertyList = mapping.types[term].properties,
 		propertiesAndTypes = {},
 		i;
+	if (mapping.types[term].specific_properties) {
+		propertyList.concat(mapping.types[term].specific_properties);
+	}
 	for (i = 0; i < propertyList.length; i += 1) {
 		propertiesAndTypes[propertyList[i]] = mapping.properties[propertyList[i]].ranges;
 	}
