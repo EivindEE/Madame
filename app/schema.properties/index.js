a95cf3f6647676629a843d919799592cb6c7e0c8
Eivind Eidheim Elseth <eivind.elseth@gmail.com>
2013-03-12 12:29:42 +0100

    Ordered properties by name before callback

--- app/schema.properties/index.js
+++ app/schema.properties/index.js
@@ -4,7 +4,9 @@ var mapping = require('./mapping').declaration;
 exports.propertiesList = function (term, callback) {
 	var propertyList = mapping.types[term].properties,
 		propertiesAndTypes = {},
-		i;
+		i,
+		keys,
+		orderedPropertiesAndTypes = {};
 	if (mapping.types[term].specific_properties) {
 		propertyList.concat(mapping.types[term].specific_properties);
 	}
@@ -14,6 +16,11 @@ exports.propertiesList = function (term, callback) {
 			'comment': mapping.properties[propertyList[i]].comment_plain
 		};
 	}
-	callback(null, propertiesAndTypes);
+	keys = Object.keys(propertiesAndTypes);
+	keys.sort();
+	for (i = 0; i < keys.length; i += 1) {
+		orderedPropertiesAndTypes[keys[i]] = propertiesAndTypes[keys[i]];
+	}
+	callback(null, orderedPropertiesAndTypes);
 };
 
