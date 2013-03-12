eb31c1dd2e62ae0cf68e465b4a72bee2b9cee341
Eivind Eidheim Elseth <eivind.elseth@gmail.com>
2013-03-12 12:09:59 +0100

    Replaced loop which could cause infinite loop with if. Removes one leading and trailing whitespace instead of all

--- public/js/sw-madame.js
+++ public/js/sw-madame.js
@@ -574,11 +574,12 @@ madame.legalRange = function (range) {
 		child,
 		start,
 		end;
-	while (newRange.toString()[0] === ' ') {
-		newRange.setStart(range.startContainer, range.startOffset + 1);
+
+	if (newRange.toString()[0] === ' ') {
+		newRange.setStart(newRange.startContainer, newRange.startOffset + 1);
 	}
-	while (newRange.toString()[newRange.toString().length - 1] === ' ') {
-		newRange.setEnd(range.endContainer, range.endOffset - 1);
+	if (newRange.toString()[newRange.toString().length - 1] === ' ') {
+		newRange.setEnd(newRange.endContainer, newRange.endOffset - 1);
 	}
 	if ((range.startContainer === range.endContainer) || (range.startContainer.parentNode === range.endContainer.parentNode)) {
 		return newRange;
