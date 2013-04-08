12bd4e9438e5798edde6172c41eaf90652a46610
Eivind Eidheim Elseth <eivind.elseth@gmail.com>
2013-04-08 16:00:09 +0200

    Removed unused code from app/disambiguate

--- app/disambiguate/index.js
+++ app/disambiguate/index.js
@@ -2,102 +2,12 @@
 'use strict';
 var http = require("http"),
 	url = require('url'),
-	properties = require('../schema.properties/mapping').declaration.properties,
-	datatypes = require('../schema.properties/mapping').declaration.datatypes,
-	types = require('../schema.properties/mapping').declaration.types,
 	saneString = function (searchString, callback) {
 		if (searchString.length > 60) {
 			callback(new Error('Search string <' + searchString + '> was to long'));
 		} else {
 			callback(null, searchString);
 		}
-	},
-	findSchemaDatatypes = function (searchTerm, callback) {
-		var schema_senses = [],
-			sense,
-			termName,
-			term;
-		try {
-			for (termName in datatypes) {
-				if (datatypes.hasOwnProperty(termName)) {
-					term = datatypes[termName];
-					if (term.label.search(new RegExp("(\\s|^)" + searchTerm, "i")) !== -1) {
-						sense = {};
-						sense.senseid = "http://schema.org/" + term.id;
-						sense.explanation = term.comment;
-						schema_senses.push(sense);
-					}
-				}
-			}
-		} catch (e) {
-		}
-		callback(schema_senses);
-	},
-	findSchemaProperties = function (searchTerm, callback) {
-		var schema_senses = [],
-			sense,
-			termName,
-			term;
-		try {
-			for (termName in properties) {
-				if (properties.hasOwnProperty(termName)) {
-					term = properties[termName];
-					if (term.label.search(new RegExp("(\\s|^)" + searchTerm, "i")) !== -1) {
-						sense = {};
-						sense.senseid = "http://schema.org/" + term.id;
-						sense.explanation = term.comment;
-						schema_senses.push(sense);
-					}
-				}
-			}
-		} catch (e) {
-		}
-		callback(schema_senses);
-	},
-	findSchemaTypes = function (searchTerm, callback) {
-		var schema_senses = [],
-			sense,
-			termName,
-			term;
-		try {
-			for (termName in types) {
-				if (types.hasOwnProperty(termName)) {
-					term = types[termName];
-					if (term.label.search(new RegExp("(\\s|^)" + searchTerm, "i")) !== -1) {
-						sense = {};
-						sense.senseid = "http://schema.org/" + term.id;
-						sense.explanation = term.comment;
-						schema_senses.push(sense);
-					}
-				}
-			}
-		} catch (e) {
-		}
-		callback(schema_senses);
-	},
-	schemaRunner = function (run, q, callback) {
-		var i,
-			counter = 0,
-			matches = [],
-			onMatch = function (matchList) {
-				counter += 1;
-				matches = matchList.concat(matches);
-				if (counter >= run.length) {
-					callback(matches);
-				}
-			};
-		for (i = 0; i < run.length; i += 1) {
-			run[i](q, onMatch);
-		}
-	},
-	findSchemaTerms = function (searchString, callback) {
-		var searchTerm;
-		searchTerm = searchString;
-		searchTerm = searchTerm.replace(/s$/, "s?"); // Catches a lot of plurals and makes them optional
-		searchTerm = searchTerm.replace(/\./g, "\\.");
-		schemaRunner([findSchemaDatatypes, findSchemaProperties, findSchemaTypes], searchTerm, function (schema_senses) {
-			callback({senses: schema_senses});
-		});
 	},
 	findLexitagTerms = function (searchString, callback) {
 		var body = '',
@@ -125,22 +35,6 @@ var http = require("http"),
 					});
 			}
 		});
-	},
-	runQueries = function (run, query, callback) {
-		var i,
-			counter = 0,
-			json = {word: '', senses: []},
-			onComplete = function (jsonResult) {
-				counter += 1;
-				json.word = jsonResult.word || json.word;
-				json.senses = jsonResult.senses ? json.senses.concat(jsonResult.senses) : json.senses;
-				if (counter === run.length) {
-					callback(json);
-				}
-			};
-		for (i = 0; i < run.length; i += 1) {
-			run[i](query, onComplete);
-		}
 	};
 
 /**
@@ -160,7 +54,7 @@ var http = require("http"),
 *	}
 */
 exports.term = function (term, callback) {
-	runQueries([findLexitagTerms], term, function (json) {
+	findLexitagTerms(term, function (json) {
 		callback(null, json);
 	});
 };
