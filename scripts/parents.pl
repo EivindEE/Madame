873f987652513be448e626e324c9baaa9c6c5801
Eivind Eidheim Elseth <eivind.elseth@gmail.com>
2013-04-29 14:38:45 +0200

    Renamed variables to reflect use in WordNet and thesis

--- scripts/parents.pl
+++ scripts/parents.pl
@@ -9,18 +9,18 @@ my $wn = WordNet::QueryData->new(
 my @stripped = ($ARGV[0] =~ /.*-(.*)-(.).*-(.*)/);
 my $synset = join('#', @stripped);
 
-my @parents = parent($synset);
-my @all_parents;
-while ( @parents){ # Does a breadth first search through the WordNet graph, can be switched to depth first by substituting shift with pop
-	my $parent = shift @parents;
-	push(@parents, parent($parent));
-	push(@all_parents, $parent);
+my @hypernym = hypernym($synset);
+my @hypernyms;
+while ( @hypernym){ # Does a breadth first search through the WordNet graph, can be switched to depth first by substituting shift with pop
+	my $hypernym = shift @hypernym;
+	push(@hypernym, hypernym($hypernym));
+	push(@hypernyms, $hypernym);
 }
 
-my @all_parents_with_siblings;
-push(@all_parents_with_siblings, {"synset" => $_ , "offset" => $wn->offset($_) ,"siblings" => [siblings($_)]}) for @all_parents;
+my @hypernyms_with_siblings;
+push(@hypernyms_with_siblings, {"synset" => $_ , "offset" => $wn->offset($_) ,"siblings" => [siblings($_)]}) for @hypernyms;
 
-my %json = ("chain" => [uniq(@all_parents_with_siblings)], "synset" => $synset, "offset"=> $wn->offset($synset), "siblings" => [siblings($synset)]);
+my %json = ("chain" => [uniq(@hypernyms_with_siblings)], "synset" => $synset, "offset"=> $wn->offset($synset), "siblings" => [siblings($synset)]);
 my $json = JSON->new->utf8->pretty->encode(\%json);
 print $json;
 
@@ -37,21 +37,21 @@ sub uniq { # Copied from http://stackoverflow.com/questions/7651/how-do-i-remove
     return @r;
 }
 
-sub parent {
-	my @parents = $wn->querySense($_[0], "hype");
-	if ($#parents > 0) {
+sub hypernym {
+	my @hypernym = $wn->querySense($_[0], "hype");
+	if ($#hypernym > 0) {
 		return ();
 	}
-	return @parents;
+	return @hypernym;
 }
 
 
 sub siblings {
 	my $synset = $_[0];
-	my @parents = parent($synset); # Find parents of synset
+	my @hypernym = hypernym($synset); # Find hypernym of synset
 	my @siblings;
 	my @sibling_and_offset;
-	push(@siblings, $wn->querySense($_, "hypo")) for @parents;
+	push(@siblings, $wn->querySense($_, "hypo")) for @hypernym;
 	my $index = 0;
 	for my $value (@siblings) { # Removes term from siblings
 		if ($value eq $synset) {
